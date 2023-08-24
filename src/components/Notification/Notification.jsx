d9f0788432677ba5b1c52103f417152343c5e6cc
AndreeaSichitiu <sichitiuandreea@gmail.com>
2023-08-24 10:05:55 +0300

    final update

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import style from "./Notification.module.css"
+import style from './Notification.module.css';
 
-export default function Notification({message}) {
-  return (
-   <p className={style.message}>{message}</p>
-  )
+export default function Notification({ message }) {
+  return <p className={style.message}>{message}</p>;
 }
- 
+
 Notification.propTypes = {
-    message: PropTypes.string.isRequired
-}
+  message: PropTypes.string.isRequired,
+};
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 import PropTypes from 'prop-types';
-import style from "./Section.module.css"
+import style from './Section.module.css';
 
-export default function Section({title, children}) {
+export default function Section({ title, children }) {
   return (
     <section className={style.feedbackWrapper}>
-    <h1 className={style.feedbackTitle}>{title}</h1>
-    {children}
-  </section>
-  )
+      <h1 className={style.feedbackTitle}>{title}</h1>
+      {children}
+    </section>
+  );
 }
 
 Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
-  };
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
